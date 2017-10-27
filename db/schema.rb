4e18e0af3446f0d47ac9228c88e6d533abbaf5db
Luwam Tesfamariam <luwamsolomon11@gmail.com>
2017-10-27 21:13:41 +0000

    pins added by scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024233043) do
+ActiveRecord::Schema.define(version: 20171026211520) do
 
-  create_table "users", force: :cascade do |t| # Here's where the problem was, I changed 'change_table' to 'create_table' *MM
+  create_table "pins", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
